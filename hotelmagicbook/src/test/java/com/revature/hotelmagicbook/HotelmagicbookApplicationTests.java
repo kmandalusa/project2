de1312bca77f9c649f5b362bffbb8649d56b4b4b
airmanon <anjali.ghosh@revature.net>
2021-11-03 14:54:56 -0700

    Create backend project
    Created the backend project for Hotel Magic Book

---
+++ hotelmagicbook/src/test/java/com/revature/hotelmagicbook/HotelmagicbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.hotelmagicbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelmagicbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
